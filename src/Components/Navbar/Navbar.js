60aa33ccc997b771a9285359d92d28a90fa11575
practice404 <hawkempire007@gmail.com>
2021-10-04 18:39:10 +0530

    navbar replaced by sidebar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import imagered from "../../assets/favicon.png";
 import imageblack from "../../assets/jkosc-logo-1.png";
-import { Link } from 'react-router-dom'
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -22,51 +22,57 @@ function Navbar() {
     } else {
       setNavblack(false);
     }
-  }
+  };
 
   window.addEventListener("scroll", changeNavColor);
   window.addEventListener("scroll", changeNavBlack);
 
   return (
-      <div className="navbar">
-        <div className={navbar ? (navblack ? "header black" : "header sticky" ) : "header"}>
+    <div className="navbar">
+      <div
+        className={
+          navbar ? (navblack ? "header black" : "header sticky") : "header"
+        }
+      >
         {/* <div className={navbar ? "header sticky" : "header"}> */}
         {/* <div className="header" > */}
-          <a className="logo">
-            {navblack ? 
-            <img className="image" src={imageblack}  /> :
-            <img className="image" src={imagered}  /> }
-          </a>
-          <ul>
-            <li>
+        <a className="logo">
+          {navblack ? (
+            <img className="image" src={imageblack} />
+          ) : (
+            <img className="image" src={imagered} />
+          )}
+        </a>
+        <ul>
+          <li>
             <Link to="/">
               <a>Home</a>
-              </Link>
-            </li>
-            <li>
-            <Link to="/incept" >
+            </Link>
+          </li>
+          <li>
+            <Link to="/incept">
               <a>Incept</a>
-              </Link>
-            </li>
-            <li>
-            <Link to="/bloom" >
+            </Link>
+          </li>
+          <li>
+            <Link to="/bloom">
               <a>Bloom Up</a>
-              </Link>
-            </li>
-            <li>
-            <Link to="/about" >
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
               <a>About Us</a>
-              </Link>
-            </li>
-            <li>
-            <Link to="/alumni" >
+            </Link>
+          </li>
+          <li>
+            <Link to="/alumni">
               <a>Our Alumni</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        </div>
-        );
-    }
-    
-    export default Navbar;
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
